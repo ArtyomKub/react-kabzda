e86475fb9c62b6e8ec888b4b38a9c34507d450f2
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-15 12:01:53 +0200

    Start to learn React with TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,54 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div>Hello</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+let a = {
+    name: 'Arsem',
+    age: 10,
+    address: {
+        city: 'Minsk',
+        country: 'Poland'
+    }
+}
+
+let users = [
+    {
+        name: 'Arsem',
+        age: 10,
+        address: {
+            city: 'Poznan',
+            country: 'Poland'
+        }
+    },
+    {
+        name: 'Viktor',
+        age: 13,
+        address: {
+            city: 'Minsk',
+            country: 'Poland'
+        }
+    },
+    {
+        name: 'Dimych',
+        age: 23,
+        address: {
+            city: 'Minsk',
+            country: 'Poland'
+        }
+    }
+
+]
+
+
+console.log(users[0].address.city)
