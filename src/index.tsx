db7771e42aa09da11618fd412cfcb91d98307124
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-19 22:11:50 +0200

    Create a new component Rating and connection in App

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import App, {Rating} from './App';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <div>Hello</div>
+    <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
@@ -15,47 +16,47 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-
-let a = {
-    name: 'Arsem',
-    age: 10,
-    address: {
-        city: 'Minsk',
-        country: 'Poland'
-    }
-}
-
-let users = [
-    {
-        name: 'Arsem',
-        age: 10,
-        address: {
-            city: 'Poznan',
-            country: 'Poland'
-        }
-    },
-    {
-        name: 'Viktor',
-        age: 13,
-        address: {
-            city: 'Minsk',
-            country: 'Poland'
-        }
-    },
-    {
-        name: 'Dimych',
-        age: 23,
-        address: {
-            city: 'Minsk',
-            country: 'Poland'
-        }
-    }
-]
-
-let b: Array<string | number>;
-b=[1,2,3,4,5,6,7,8,9,10];
-b=[]
-b=[1,2,3,4,5,6,7]
-b=[123,12,124,'ted',12,'dog',2134]
-
-console.log(b)
+//
+// let a = {
+//     name: 'Arsem',
+//     age: 10,
+//     address: {
+//         city: 'Minsk',
+//         country: 'Poland'
+//     }
+// }
+//
+// let users = [
+//     {
+//         name: 'Arsem',
+//         age: 10,
+//         address: {
+//             city: 'Poznan',
+//             country: 'Poland'
+//         }
+//     },
+//     {
+//         name: 'Viktor',
+//         age: 13,
+//         address: {
+//             city: 'Minsk',
+//             country: 'Poland'
+//         }
+//     },
+//     {
+//         name: 'Dimych',
+//         age: 23,
+//         address: {
+//             city: 'Minsk',
+//             country: 'Poland'
+//         }
+//     }
+// ]
+//
+// let b: Array<string | number>;
+// b=[1,2,3,4,5,6,7,8,9,10];
+// b=[]
+// b=[1,2,3,4,5,6,7]
+// b=[123,12,124,'ted',12,'dog',2134]
+//
+// console.log(b)
