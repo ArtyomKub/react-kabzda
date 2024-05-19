6eca52a8a37f8f724291710d881d7906790a2d18
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-19 23:57:49 +0200

    Refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,28 @@ import './App.css';
 import {Accordion} from './components/Accordion/Accordion';
 import {Rating} from './components/Rating/Rating';
 
-export function App() {
-    return (
-        <div className="App">
-            <PageTitle title = {'This is APP component'}/>
-            <PageTitle title = {'Fucking sheet'}/>
-            <Accordion titleValue = {'MENU'}/>
-            <Accordion titleValue = {'USERS'}/>
-            <Rating value = {0}/>
-            <Rating value = {1}/>
-            <Rating value = {2}/>
-            <Rating value = {3}/>
-            <Rating value = {4}/>
-            <Rating value = {5}/>
-        </div>
-    );
-}
-
 type PageTitlePropsType = {
     title: string
 }
 
-export function PageTitle(props: PageTitlePropsType) {
+export function App() {
+    return (
+        <div className="App">
+            <PageTitle title={'This is APP component'}/>
+            <Accordion titleValue={'MENU'}/>
+            <Accordion titleValue={'USERS'}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>
+    );
+}
+
+
+function PageTitle(props: PageTitlePropsType) {
     return (
         <h1>{props.title}</h1>
     )
