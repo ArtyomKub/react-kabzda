920bb54127e20ca79ef0e963b65a08f7b04a6122
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-06-14 13:10:32 +0200

    Create a two button

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -3,9 +3,13 @@ import React from 'react';
 const OnOff = () => {
     return (
         <div>
-
+            <button>On</button>
+            <button>Off</button>
         </div>
     );
 };
+
+//true - green button and circle
+// false - red button and circle
 
 export default OnOff;
